5367885e9e2e8a7f74f935ce8ce11775dda8463b
omae-muds <57761233+omae-muds@users.noreply.github.com>
2020-10-01 19:29:08 +0900

    read timetable file

--- main.go
+++ main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World!")
+func ttprinter(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadFile("timetable90.txt")
+	if err != nil {
+		l := log.New(os.Stderr, "", 1)
+		l.Println(err)
+		os.Exit(1)
+	}
+	lines := string(b)
+	io.WriteString(w, lines)
 }
 
 func main() {
 	port := os.Getenv("PORT")
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", ttprinter)
 	http.ListenAndServe(":"+port, nil)
 }
